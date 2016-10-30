3bfb3fe4bd74aaf0cdc3eb1e1f900f13f6b970b5
Chris Daly <cmpcdaly@gmail.com>
2016-10-30 10:30:06 +0000

    Change endpoint to take hex string as encrypted key.

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -8,12 +8,15 @@ var router = express.Router();
 router.get('/decrypt', function(req, res) {
   var calculator = new DrunkCalculator();
   var username = req.query.user;
-  var key = req.query.key;
+  var key = parseInt(req.query.key, 16);
 
-  var user = users.getById(username);  
+  var user = users.getById(username);
+
+  res.set('Content-Type', 'application/json');
 
   calculator.calculate(user, key, function(result){
-    if (result.isDrunkEnough && result.isValid)  { 
+    if (result.isDrunkEnough && result.isValid)  {
+      console.log(result.isDrunkEnough, result.isValid, result.alcoholLevel);
       res.status(200).send({
         alcoholLevel: result.alcoholLevel
       });
@@ -23,7 +26,7 @@ router.get('/decrypt', function(req, res) {
         alcoholLevel: result.alcoholLevel
       });
     }
-  })
+  });
 });
 
 module.exports = exports = router;
